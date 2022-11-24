a4d907aed08f653f3578780815d0d3e4e8a23ae3
jmb-oddbook <oddbook@posteo.net>
2022-11-24 19:28:43 +0100

    update caesar with punctuation and umlauts

--- python/100days/caesar.py
+++ python/100days/caesar.py
@@ -4,25 +4,26 @@
 
 # Caesar Cipher
 
-alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',\
+         'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',\
+         'ä', 'ö', 'ü', 'ß']
 
 def caesar_cypher(dir, plaintext, shift_nr):
     cypher_text = ""
-    if dir == "encode":
-        for letter in plaintext:
-            new_position = (alpha.index(letter) + shift_nr)%26
+    if dir == "decode":
+        shift_nr *= -1
+    for char in plaintext:
+        if char in alpha:
+            new_position = (alpha.index(char) + shift_nr)%30
             cypher_text += alpha[new_position]
-    else:
-        for letter in plaintext:
-            new_position = (alpha.index(letter) - shift_nr)%26
-            cypher_text += alpha[new_position]
+        else:
+            cypher_text += char
     return cypher_text
     
 again = "yes"
 
 while again == "yes":
     direction = input("Type 'encode', or 'decode': ")
-    # ToDo: regex filter for punctuation
     message = input("Type your message: ").lower()
     shift = int(input("Type the shift number: "))
     
